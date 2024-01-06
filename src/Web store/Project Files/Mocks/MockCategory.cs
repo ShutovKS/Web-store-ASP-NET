d4d91e0a6b9ba9d69a410da13d5510236e17b40d
ShutovKS <i@shutovks.ru>
2024-01-06 20:26:17 +0300

    Added mock data

---
+++ src/Web store/Project Files/Mocks/MockCategory.cs
@@ -0,0 +1,19 @@
+﻿using Web_store.Models;
+using Web_store.Project_Files.Interfaces;
+
+namespace Web_store.Project_Files.Mocks;
+
+public class MockCategory : IItemsCategory
+{
+    public IEnumerable<Category> AllCategories
+    {
+        get
+        {
+            return new List<Category>
+            {
+                new() { Name = "Мечи", Description = "Короткие, длинные, кривые, прямые, двуручные, одноручные..." },
+                new() { Name = "Щиты", Description = "Круглые, треугольные, квадратные, прямоугольные, треугольные, овальные..." },
+            };
+        }
+    }
+}
