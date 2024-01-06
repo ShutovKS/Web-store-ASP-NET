4d112155b8294ecb06c8e8d618f917f767dcefff
ShutovKS <i@shutovks.ru>
2024-01-06 20:25:33 +0300

    Added interfaces AllItem and ItemsCategory

---
+++ src/Web store/Project Files/Interfaces/IAllItem.cs
@@ -0,0 +1,10 @@
+﻿using Web_store.Models;
+
+namespace Web_store.Project_Files.Interfaces;
+
+public interface IAllItem
+{
+    IEnumerable<Item> Item { get; }
+    IEnumerable<Item> GetFavItem { get; }
+    Item GetObjectItem(int id);
+}
---
+++ src/Web store/Project Files/Interfaces/IItemsCategory.cs
@@ -0,0 +1,8 @@
+﻿using Web_store.Models;
+
+namespace Web_store.Project_Files.Interfaces;
+
+public interface IItemsCategory
+{
+    IEnumerable<Category> AllCategories { get; }
+}
