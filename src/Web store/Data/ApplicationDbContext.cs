a7061f59ae1562bb20f179d4a0ed34055bb5c9d3
ShutovKS <schutow.kir@yandex.ru>
2024-02-01 15:37:47 +0300

    Added order data

--- src/Web store/Data/ApplicationDbContext.cs
+++ src/Web store/Data/ApplicationDbContext.cs
@@ -5,12 +5,14 @@ namespace Web_store.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext() {}
-        
+        public ApplicationDbContext() { }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Item> Item { get; set; }
         public DbSet<Category> Category { get; set; }
         public DbSet<ItemInCart> ItemInCart { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
     }
 }
---
+++ src/Web store/Data/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace Web_store.Data.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
